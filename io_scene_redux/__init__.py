2830c6690b3b6a4e7cb29575921da4627c8c96e5
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-09-22 22:42:45 +0400

    Corrected out info

--- io_scene_redux/__init__.py
+++ io_scene_redux/__init__.py
@@ -1,10 +1,10 @@
 bl_info = {
-    "name": "Redux SDK Tools",
-    "author": "blender-xray contributors; MetroFormats contributors",
+    "name": "4A Engine SDK Tools",
+    "author": "APAMk2",
     "version": (0, 6, 7),
     "blender": (3, 2, 0),
     "location": "File > Import/Export",
-    "description": "Import and export Redux SDK models, skeletons and compiled motions",
+    "description": "Import and export 4A Engine SDK models, skeletons and compiled motions",
     "category": "Import-Export",
 }
 
