f992460b07150539bd6c688c3b28feaf9520bb33
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-09-24 14:51:37 +0400

    Flip X axis for models

--- io_scene_redux/utils/axis.py
+++ io_scene_redux/utils/axis.py
@@ -1,25 +1,25 @@
-"""uEngine +Z forward/+Y up to Blender -Y forward/+Z up."""
+"""Convert between uEngine's left-handed Y-up basis and Blender."""
 
 from mathutils import Matrix, Vector
 
 
-BASIS_ID = "X_NEGZ_Y"
+BASIS_ID = "X_Z_Y"
 STATIC_BASIS_ID = "X_Z_Y"
 
 
 def to_blender_basis():
     return Matrix(((1, 0, 0, 0),
-                   (0, 0, -1, 0),
+                   (0, 0, 1, 0),
                    (0, 1, 0, 0),
                    (0, 0, 0, 1)))
 
 
 def to_blender_vector(value):
-    return Vector((value[0], -value[2], value[1]))
+    return Vector((value[0], value[2], value[1]))
 
 
 def to_redux_vector(value):
-    return Vector((value[0], value[2], -value[1]))
+    return Vector((value[0], value[2], value[1]))
 
 
 def to_blender_static_vector(value):
