494cbe89b27c540d1fe19855dee78e190392c484
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-09-24 16:28:37 +0400

    Version bump

--- io_scene_redux/__init__.py
+++ io_scene_redux/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "4A Engine SDK Tools",
     "author": "APAMk2",
-    "version": (0, 6, 7),
+    "version": (0, 7, 0),
     "blender": (3, 2, 0),
     "location": "File > Import/Export",
     "description": "Import and export 4A Engine SDK models, skeletons and compiled motions",
